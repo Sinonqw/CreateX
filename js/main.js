8dfe52a56a76b74f190c2e01e06bf079b74a9174
George Zaklepniy <zak.taio2@gmail.com>
2024-06-30 13:11:44 +0300

    Добавлено открытие оверлея только при нажатии на мобильное меню

--- js/main.js
+++ js/main.js
@@ -1,13 +1,12 @@
 $(function () {
+	var mixer = mixitup('.directions-list')
 
-  var mixer = mixitup('.directions-list');
+	$('.directions-filter-btn').on('click', function name() {
+		$('.directions-filter-btn').removeClass('directions-filter-btn--active')
+		$(this).addClass('directions-filter-btn--active')
+	})
 
-  $('.directions-filter-btn').on('click', function name() {
-    $('.directions-filter-btn').removeClass('directions-filter-btn--active')
-    $(this).addClass('directions-filter-btn--active');
-  })
-
-  $('.team-slider').slick({
+	$('.team-slider').slick({
 		arrows: false,
 		slidesToShow: 4,
 		draggable: false,
@@ -34,16 +33,16 @@ $(function () {
 			},
 		],
 	})
-  $('.team-slider-prev').on('click', function(e) {
-    e.preventDefault()
-    $('.team-slider').slick('slickPrev')
-  })
-  $('.team-slider-next').on('click', function(e) {
-    e.preventDefault()
-    $('.team-slider').slick('slickNext')
-  })
+	$('.team-slider-prev').on('click', function (e) {
+		e.preventDefault()
+		$('.team-slider').slick('slickPrev')
+	})
+	$('.team-slider-next').on('click', function (e) {
+		e.preventDefault()
+		$('.team-slider').slick('slickNext')
+	})
 
-  $('.testimonials-slider').slick({
+	$('.testimonials-slider').slick({
 		arrows: false,
 		draggable: false,
 		dots: true,
@@ -52,63 +51,60 @@ $(function () {
 		responsive: [
 			{
 				breakpoint: 700,
-				settings: {
-					
-				},
+				settings: {},
 			},
 		],
 	})
-    $('.testimonials-prev').on('click', function (e) {
-			e.preventDefault()
-			$('.testimonials-slider').slick('slickPrev')
-		})
-		$('.testimonials-next').on('click', function (e) {
-			e.preventDefault()
-			$('.testimonials-slider').slick('slickNext')
-		})
+	$('.testimonials-prev').on('click', function (e) {
+		e.preventDefault()
+		$('.testimonials-slider').slick('slickPrev')
+	})
+	$('.testimonials-next').on('click', function (e) {
+		e.preventDefault()
+		$('.testimonials-slider').slick('slickNext')
+	})
 
-		$('.program-acc-link').on('click', function(e) {
-			e.preventDefault()
-			$(this).toggleClass('program-acc-link--active')
-			$(this).children('.program-acc-text').slideToggle()
-		})
+	$('.program-acc-link').on('click', function (e) {
+		e.preventDefault()
+		$(this).toggleClass('program-acc-link--active')
+		$(this).children('.program-acc-text').slideToggle()
+	})
 
-		$(
-			'.header-nav-list a, .header-top-btn, .footer-go-top, .footer-item-link'
-		).on('click', function (e) {
-			e.preventDefault()
-			var id = $(this).attr('href'),
-				top = $(id).offset().top
-			$('body,html').animate({ scrollTop: top }, 1000)
-		})
+	$(
+		'.header-nav-list a, .header-top-btn, .footer-go-top, .footer-item-link'
+	).on('click', function (e) {
+		e.preventDefault()
+		var id = $(this).attr('href'),
+			top = $(id).offset().top
+		$('body,html').animate({ scrollTop: top }, 1000)
+	})
 
-		$('.burger, .overlay, .header-top a').on('click', function (e) {
-			e.preventDefault()
-			$('.header-top-inner').toggleClass('header-top-open')
-			$('.overlay').toggleClass('overlay--show')
-			$('.close-btn').toggleClass('close')
-			$('.burger').toggleClass('active')
-		})
+	$('.burger').on('click', function (e) {
+		e.preventDefault()
+		$('.header-top-inner').toggleClass('header-top-open')
+		$('.overlay').toggleClass('overlay--show')
+		$('.close-btn').toggleClass('close')
+		$('.burger').toggleClass('active')
+	})
 
-		setInterval(() => {
-			if($('span').hasClass('close')) {
-				$('.burger').css('')
-			}
-		}, 0);
+	setInterval(() => {
+		if ($('span').hasClass('close')) {
+			$('.burger').css('')
+		}
+	}, 0)
 
-		setInterval(() => {
-			if ($(window).scrollTop() > 0 && $('.header-top-inner').hasClass('header-top-open') === false) {
-				$('.burger').addClass('burger--follow')
-			} else {
-				$('.burger').removeClass('burger--follow')
-			}
-		}, 0);
+	setInterval(() => {
+		if (
+			$(window).scrollTop() > 0 &&
+			$('.header-top-inner').hasClass('header-top-open') === false
+		) {
+			$('.burger').addClass('burger--follow')
+		} else {
+			$('.burger').removeClass('burger--follow')
+		}
+	}, 0)
 
-		$('.footer-top-title--slide').on('click', function () {
-			$(this).next().slideToggle()
-		})
-
+	$('.footer-top-title--slide').on('click', function () {
+		$(this).next().slideToggle()
+	})
 })
-
-
-
